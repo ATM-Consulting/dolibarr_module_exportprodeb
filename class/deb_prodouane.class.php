e2b0837852594aa7e49878bc9e60c9299d0f7a07
gauthier <gauthier.verdol@atm-consulting.fr>
2016-12-01 16:49:28 +0100

    NEW : entete OK

--- class/deb_prodouane.class.php
+++ class/deb_prodouane.class.php
@@ -14,38 +14,61 @@ class TDebProdouane extends TObjetStd {
 		
 	}
 	
-	function getXML() {
+	
+	/**
+	 * @param $mode O pour création, R pour régénération
+	 * @param $type introduction ou expedition
+	 */
+	function getXML($mode='O', $type='introduction', $periode_reference='') {
 
-		global $db, $conf;
-
+		global $db, $conf, $mysoc;
+		
+		/**************Construction de quelques variables********************/
+		$party_id = substr(strtr($mysoc->tva_intra, array(' '=>'')), 0, 4).$mysoc->idprof2;
+		$declarant = substr($mysoc->managers, 0, 14);
+		$id_declaration = self::getLastIdDeclaration();
+		/********************************************************************/
+		
+		/**************Construction du fichier XML***************************/
 		$e = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><INSTAT></INSTAT>');
+		
 		$enveloppe = $e->addChild('Envelope');
-		
-		// balise envelopeId
 		$enveloppe->addChild('envelopeId', $conf->global->EXPORT_PRO_DEB_NUM_AGREMENT);
-		
-		// Balise DateTime
 		$date_time = $enveloppe->addChild('DateTime');
 		$date_time->addChild('date', date('Y-m-d'));
 		$date_time->addChild('time', date('H:i:s'));
-		
-		// Balise Party
 		$party = $enveloppe->addChild('Party partType="'.$conf->global->EXPORT_PRO_DEB_TYPE_ACTEUR.'" partyRole="'.$conf->global->EXPORT_PRO_DEB_ROLE_ACTEUR.'"');
-		$party->addChild('partyId', 'coucou');
-		$party->addChild('partyName', 'coucou');
-		
-		// Balise software
+		$party->addChild('partyId', $party_id);
+		$party->addChild('partyName', $declarant);
 		$enveloppe->addChild('softwareUsed', 'Dolibarr');
-		/*$e->addChild('Name', utf8_encode($product['label']));
-		$price = $e->addChild('Price');
-		$price->addChild('Value', $TPrices['price_patient']);
-		$price1 = $e->addChild('Price1');
-		$price1->addChild('Value', $TPrices['price_pro']);
-		$e->addChild('TaxRate', $TPrices['tva']);
-		$s = $e->addChild('QuantityInStock');
-		$s->addChild('Value', $stock);*/
+		$declaration = $enveloppe->addChild('Declaration');
+		$declaration->addChild('declarationId', $id_declaration);
+		$declaration->addChild('referencePeriod', $periode_reference);
+		if($conf->global->EXPORT_PRO_DEB_TYPE_ACTEUR === 'PSI') $psiId = $party_id;
+		else $psiId = 'NA';
+		$declaration->addChild('PSIId', $psiId);
+		$function = $declaration->addChild('Function');
+		$functionCode = $function->addChild('functionCode', $mode);
+		$declaration->addChild('declarationTypeCode', $conf->global->{'EXPORT_PRO_DEB_NIV_OBLIGATION_'.strtoupper($type)});
+		$declaration->addChild('flowCode', ($type == 'introduction' ? 'A' : 'D'));
+		$declaration->addChild('currencyCode', $conf->global->MAIN_MONNAIE);
+		/********************************************************************/
 		
-		var_dump($e->asXML());
+		self::getItems($declaration);
+		
+		pre($e->asXML(), true);
+		
+	}
+	
+	private function getItems(&$declaration) {
+		
+		
+		
+	}
+	
+	function getLastIdDeclaration() {
+		
+		
 		
 	}
 	
